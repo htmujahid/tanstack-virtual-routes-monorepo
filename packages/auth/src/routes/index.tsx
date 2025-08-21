54e297366669e60e161a1e04f23300838cced309
Talha Mujahid <htmujahid@gmail.com>
2025-08-21 12:12:03 +0500

    Create index.tsx

---
+++ packages/auth/src/routes/index.tsx
@@ -0,0 +1,12 @@
+import { createFileRoute, Outlet } from '@tanstack/react-router'
+
+export const Route = createFileRoute('/auth/')({
+  component: RouteComponent,
+})
+
+function RouteComponent() {
+    return (<div>
+      <h1>Hell form /auth/</h1>
+
+    </div>)
+}
